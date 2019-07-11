f79cd6b6f0a64ebdc239116e7d9a009f85dbf4bc
jamesbyrne89 <mail@jamestbyrne.com>
2019-07-11 22:52:12 +0100

    Add Vue input component

---
+++ client/src/components/Input.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input
+      class="url-input"
+      placeholder="Enter a url to shorten..."
+      @input="handleInput"
+      v-model="input"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  data: () => ({
+    input: ""
+  }),
+  methods: {
+    handleInput: e => {
+      // eslint-disable-next-line
+      console.log(e.target.value);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
